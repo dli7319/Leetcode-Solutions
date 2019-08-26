4ea95ae6de2b9a56aa1d96b027f62bfcf8ce4bc1
dli7319 <dli7319@gmail.com>
2019-08-25 21:11:59 -0400

    Add shifting-letters solution

---
+++ shifting-letters/solution.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun shiftingLetters(S: String, shifts: IntArray): String {
+        val cdf = Array(shifts.size) {0}
+        for (i in shifts.indices) {
+            cdf[i] = shifts[i].rem(26)
+        }
+        for (i in cdf.size - 2 downTo 0) {
+            cdf[i] += cdf[i+1]
+        }
+        var newString = StringBuffer()
+        for (i in 0 until S.length) {
+            newString.append('a' + (S.get(i) - 'a' + cdf[i]).rem(26))
+        }
+        return newString.toString()
+    }
+}
