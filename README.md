fb0ba3920ff5d34184036ef3ba71b991591ac2fe
dli7319 <dli7319@gmail.com>
2019-09-12 21:51:23 -0400

    Add distance-between-bus-stops

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Leetcode Solutions
 My attempts at various leetcode problems.
 They should all work but not necessarily the most optimally.
-My solutions are usually in C++, Kotlin, or JavaScript.
+I use leetcode to practice C++, Kotlin, JavaScript, and Python 3.
 
 ### Very slow solutions (to fix later)
 * binary-tree-cameras
---
+++ distance-between-bus-stops/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if (start <= destination):
+            return min(sum(distance[start:destination]),
+                   sum(distance[destination:]) + sum(distance[:start]))
+        else:
+            return min(sum(distance[destination:start]),
+                   sum(distance[start:]) + sum(distance[:destination]))
