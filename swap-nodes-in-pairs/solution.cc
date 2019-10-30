2c78924e7bc036577650693176ff4e2dfd3d0c4f
dli7319 <dli7319@gmail.com>
2019-10-29 20:49:29 -0400

    Add swap-nodes-in-pairs

---
+++ swap-nodes-in-pairs/solution.cc
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL) {
+            return NULL;
+        } else if (head->next == NULL) {
+            return head;
+        }
+        ListNode* second = head->next;
+        head->next = swapPairs(second->next);
+        second->next = head;
+        return second;
+    }
+};
